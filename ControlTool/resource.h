6a96a12bc087964e1ac7b67bd67e395322109e26
HW <srxiaoj@gmail.com>
2015-05-30 11:51:11 -0400

    Create resource.h

---
+++ ControlTool/resource.h
@@ -0,0 +1,109 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ControlTool.rc
+//
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_CONTROLTOOL_DIALOG          102
+#define IDR_MAINFRAME                   128
+#define IDD_LOCKINBOX                   135
+#define IDD_INFOBOX                     135
+#define DLN1                            1003
+#define DLN2                            1004
+#define DLN3                            1005
+#define PositionBox                     1009
+#define MovePs                          1010
+#define LockinButton                    1010
+#define MoveDLN                         1011
+#define MoveCts                         1012
+#define MovePSRel                       1014
+#define MoveType                        1014
+#define RunLinear1                      1014
+#define RunButton                       1014
+#define MoveCtsRel                      1016
+#define Infobox                         1017
+#define StartConnection                 1018
+#define ComMM3000                       1020
+#define ComMercury                      1021
+#define ComSpetrometer                  1022
+#define ComPhase                        1023
+#define ComOther                        1024
+#define Start1                          1025
+#define End1                            1026
+#define Step1                           1027
+#define RunScan                         1028
+#define LockinPort                      1029
+#define Sample                          1032
+#define Increment                       1033
+#define TheProgressBar                  1034
+#define RunScanLog                      1035
+#define DSetting                        1036
+#define Start2D2                        1037
+#define Start2A                         1037
+#define AcelerationBox                  1038
+#define End2D2                          1038
+#define End2A                           1038
+#define Step2D2                         1039
+#define Step2A                          1039
+#define SetVelocity                     1040
+#define AverageTime                     1040
+#define Direction                       1041
+#define ChangeTunning                   1042
+#define ILBox                           1046
+#define RunScan2D                       1047
+#define RunCustom                       1047
+#define DLN2D1                          1048
+#define Start2D13                       1049
+#define Start2B                         1049
+#define End2D13                         1050
+#define End2B                           1050
+#define Step2D13                        1051
+#define Step2B                          1051
+#define DLN2D3                          1052
+#define SetTC                           1053
+#define DLFactor                        1054
+#define SLFactor                        1055
+#define TConstant                       1056
+#define ScanList                        1062
+#define ScanText                        1063
+#define MMPos                           1064
+#define MercPos                         1065
+#define MMVel                           1072
+#define MMAcc                           1073
+#define MMVel2                          1074
+#define MercVel                         1074
+#define mainButton                      1082
+#define ScanLegend                      1084
+#define PAR0                            1085
+#define PAR2                            1086
+#define PAR1                            1087
+#define PAR3                            1088
+#define PAR4                            1089
+#define PAR5                            1090
+#define PAR6                            1091
+#define PAR7                            1092
+#define InfoBoxButton                   1093
+#define SP0                             1094
+#define SP1                             1095
+#define SP2                             1096
+#define SP3                             1097
+#define SP4                             1098
+#define SP5                             1099
+#define SP6                             1100
+#define SP7                             1101
+#define AuxCheck                        1105
+#define ReadLockin                      1106
+#define IDC_BUTTON3                     1107
+#define ClearScreen                     1107
+#define ClearText                       1107
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        136
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1110
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
